a2d78f355a7d39b04fd66feecf65793a572df2a5
shiv <shivjeet@capiot.com>
2019-09-26 13:32:37 +0530

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# shivbhosale.github.io
-Portfolio
---
+++ precache-manifest.ac33127e0a35adee958e5f53ba45e915.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626134be1a3300be6d4c3394edb72225",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aee5323634901eb73355",
+    "url": "/static/css/2.cc82b09a.chunk.css"
+  },
+  {
+    "revision": "61c25babccac52630056",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "aee5323634901eb73355",
+    "url": "/static/js/2.03267da8.chunk.js"
+  },
+  {
+    "revision": "61c25babccac52630056",
+    "url": "/static/js/main.ec1c3839.chunk.js"
+  },
+  {
+    "revision": "79b3bcf019e47f8675db",
+    "url": "/static/js/runtime-main.b21d89af.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
