71c37fbcfa312ff7a59ab069e1e50deadb26fbe3
shiv <shivjeet@capiot.com>
2019-11-25 19:22:17 +0530

    Added a sample blog in. Added a config file for the blog.

--- src/activities/blogDrawer.js
+++ src/activities/blogDrawer.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import {
     Drawer, Button, Radio, Icon,
-    Result,
+    Result, Card,
     Typography
 } from 'antd';
+import JupViewer from './JupViewer'
+const blogConfig = require('./blogConfig.json')
+console.log(blogConfig)
+const { Meta } = Card;
 
 export class BlogDrawer extends React.Component {
-    state = { visible: false, placement: 'right' };
+    state = {
+        visible: false,
+        placement: 'right',
+
+        // states for the Blog modal
+        bmodal_visible: false,
+        bmodal_title: "Loading...",
+        bmodal_cover: "",
+        bmodal_subtitle:"",
+        bmodal_subtext:"",
+        bmodal_nb_url:""
+
+    };
 
     showDrawer = () => {
         this.setState({
@@ -16,13 +32,31 @@ export class BlogDrawer extends React.Component {
 
     onClose = () => {
         this.setState({
-            visible: false,
+            visible: false
         });
     };
 
     onChange = e => {
         this.setState({
             placement: e.target.value,
+        });
+    };
+
+    openBlogDrawer(item) {
+        console.log(item)
+        this.setState({
+            bmodal_visible: true,
+            bmodal_title: item['title'],
+            bmodal_cover: item['cover_img_url'],
+            bmodal_subtitle:item['subtitle'],
+            bmodal_subtext:item['subtitle'],
+            bmodal_nb_url:item['nb_url']
+        })
+    }
+
+    onCloseBlog = () => {
+        this.setState({
+            bmodal_visible: false
         });
     };
 
@@ -39,19 +73,56 @@ export class BlogDrawer extends React.Component {
                     closable={false}
                     onClose={this.onClose}
                     visible={this.state.visible}
+                    // visible={true}
                     width='350px'
                 >
+
+                    {
+                        blogConfig['blogs'].map(item => (
+                            <Card
+                                onClick={()=>{this.openBlogDrawer.bind(this)(item)}}
+                                hoverable
+                                cover={<img alt="example" src={item['cover_img_url']} />}
+                            >
+                                <Meta title={item['title']} description={item['subtitle']} />
+                            </Card>
+
+                        ))
+                    }
                     <Result
                         icon={<Icon type="meh" theme="twoTone" />}
                         title="Umm... Awkward..."
                         subTitle="Hey! I'm still working on my blog"
                     />
                     <center>
-                    <Typography.Title level={4}>In the mean time</Typography.Title>
-                    <ul>
-                        <li><div><Icon type="robot"></Icon> Check out my Twitter <a href="https://twitter.com/artstylebot" target="_blank">AI Bot</a> till then!</div></li>
-                        <li><div>Click on the background pattern for an easter egg!</div></li>
-                    </ul>
+                        <Typography.Title level={4}>In the mean time</Typography.Title>
+                        <ul>
+                            <li><div><Icon type="robot"></Icon> Check out my Twitter <a href="https://twitter.com/artstylebot" target="_blank">AI Bot</a> till then!</div></li>
+                            <li><div>Click on the background pattern for an easter egg!</div></li>
+                        </ul>
+                    </center>
+                </Drawer>
+                <Drawer
+                    title="Opened Blog"
+                    placement={this.state.placement}
+                    closable={true}
+                    onClose={this.onCloseBlog}
+                    visible={this.state.bmodal_visible}
+                    width='100%'
+                    bodyStyle={{
+                        padding:'0px',
+                        backgroundColor:'#515151'
+                    }}
+                >
+                    <center>
+                        <JupViewer
+                            title={this.state.bmodal_title}
+                            subtitle={this.state.bmodal_subtitle}
+                            coverImg={this.state.bmodal_cover}
+                            // file={ipynb}
+                            file={this.state.bmodal_nb_url}
+                        // file="https://raw.githubusercontent.com/fastai/course-v3/master/nbs/dl1/00_notebook_tutorial.ipynb"
+                        />
                     </center>
                 </Drawer>
             </div>
