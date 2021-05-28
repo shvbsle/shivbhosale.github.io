a4f7d399a36dc07c59bcf18dfa5783f37d9811f2
ShivBhosale <shivbhosale97@gmail.com>
2021-05-28 12:00:02 +0530

    Updates

---
+++ precache-manifest.7ccb9630ab7bf15fd1929d342ef0c391.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aba0c80a82eb2cb4bbc013ec86f71452",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de7539035c4e7c43713d",
+    "url": "/static/css/2.929389fa.chunk.css"
+  },
+  {
+    "revision": "8bc7b27a0e766c9bb802",
+    "url": "/static/css/main.2fe01443.chunk.css"
+  },
+  {
+    "revision": "de7539035c4e7c43713d",
+    "url": "/static/js/2.2d6c24c2.chunk.js"
+  },
+  {
+    "revision": "8bc7b27a0e766c9bb802",
+    "url": "/static/js/main.6d7c42e6.chunk.js"
+  },
+  {
+    "revision": "ce6fe54a63ed48ed1db4",
+    "url": "/static/js/runtime-main.c2d91992.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
